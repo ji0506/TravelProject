ea2a64ba1c243fc2a274e5dbf91e00aa53821ee6
kjg <o_opop9999@naver.com>
2023-08-09 16:58:41 +0900

    회원가입 기능 추가

--- SpringTraveler/src/main/java/com/spr/travel/domain/User.java
+++ SpringTraveler/src/main/java/com/spr/travel/domain/User.java
@@ -1,11 +1,10 @@
 package com.spr.travel.domain;
 
-import java.util.Date;
-
-import javax.persistence.*;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @NoArgsConstructor
 @Entity
@@ -14,19 +13,23 @@ import lombok.NoArgsConstructor;
 public class User {
 
 	@Id
-	private String userId;
+	private String userId; //아이디
 
-	private String userPwd;
+	private String userPwd; //비밀번호
 
-	private String userName;
+	private String userName; //이름
 
-	private String userEmail;
+	private String userGender; //성별
 
-	private String userCp;
+	private String userBirth; //생년월일
 
-	private String userAddr;
+	private String userEmail; //이메일
 
-	private String userDaddr;
+	private String userCp; //핸드폰번호
+
+	private String userAddr; //주소
+
+	private String userDaddr; //상세주소
 
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date regDate;
