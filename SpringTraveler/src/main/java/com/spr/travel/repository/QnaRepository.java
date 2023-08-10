33efa3b0450f80db954b00e96e26fc634caf58dd
zxzx <zxzx00202@naver.com>
2023-08-10 17:56:57 +0900

    질문과 답변 글쓰기, 나의 질문 구현중

--- SpringTraveler/src/main/java/com/spr/travel/repository/QnaRepository.java
+++ SpringTraveler/src/main/java/com/spr/travel/repository/QnaRepository.java
@@ -1,13 +1,11 @@
 package com.spr.travel.repository;
 
+import com.spr.travel.domain.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spr.travel.domain.ProductDetail;
 
 
-public interface QnaRepository extends JpaRepository<ProductDetail, Integer>{
-	
-	
-	
-	
+public interface QnaRepository extends JpaRepository<Qna, Integer>{
+
 }
